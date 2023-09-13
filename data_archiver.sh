385f40050a5ffb818864b1311aa52c060da572ab
desktop <haydenguyn@gmail.com>
2023-09-13 16:52:58 -0700

    Created data archiver in Bash

---
+++ data_archiver.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# 1st argument passed will be the target directory otherwise it will be current directory
+target_directory="${1:-.}"
+
+# List all files in the target directory and subdirectories
+files_list=($(find "$target_directory" -type f))
+
+# List of file types to be backed up
+file_types=('ma' 'mb' 'drp' 'prproj' 'nk' 'aep' 'ai' 'psd')
+
+# Get the date in format YYYY_MM_DD_HH_MM_SS
+formatted_date=$(date +"%Y_%m_%d_%H_%M_%S")
+
+# The directory to copy the files will be the target_directory/data
+file_copy_directory="$target_directory/$formatted_date"
+
+# Create the directory for file copies
+mkdir -p "$file_copy_directory"
+
+# Create a log file to keep track of which files were copied
+log_file="$file_copy_directory/$formatted_date.log"
+
+# Append the text to the log file: -e to interpret newline characters
+echo -e "Files backed up:\n" >> $log_file
+
+# Iterate over every file in files_list
+for file in "${files_list[@]}"; do\
+    # Extract the file extension
+    file_extension=$(echo "$file" | awk -F '.' '{print $NF}')
+
+    # If the file extension is one of the allowed file types
+    if [[ " ${file_types[*]} " =~ " $file_extension " ]]; then
+        # Copy the file to the created directory
+        cp "$file" "$file_copy_directory"
+        # Append the name of the file to the log file
+        echo $file >> $log_file
+    fi
+done
+
+# Created a compressed zip file of the created directory with copied files
+tar -czvf "$formatted_date.tar.gz" "$file_copy_directory"
+# Delete the uncompressed folder
+rm -r "$file_copy_directory"
